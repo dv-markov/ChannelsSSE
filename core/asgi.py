cda306d23b7bc2726767aec309c776c4f829e146
dv-markov <dmitry.v.markov@gmail.com>
2023-10-29 21:20:11 +0300

    update asgi.py

--- core/asgi.py
+++ core/asgi.py
@@ -12,11 +12,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
-import chat.routing
-
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
         ),
