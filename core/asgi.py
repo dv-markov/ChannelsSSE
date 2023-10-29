0e1d2eaf8ffe4e8ad7581549ba5eb0e87c0ecc97
dv-markov <dmitry.v.markov@gmail.com>
2023-10-29 23:09:13 +0300

    adjusting wss

--- core/asgi.py
+++ core/asgi.py
@@ -14,7 +14,7 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
-        "https": django_asgi_app,
+        "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
         ),
